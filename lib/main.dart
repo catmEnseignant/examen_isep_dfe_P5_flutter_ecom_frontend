fc56f12f04271cff382e12a05c78185d893623f6
catm2017 <cheikhmbengue146@gmail.com>
2025-10-30 09:32:33 +0000

    Finish init exam flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,3 @@
-import 'package:exam_ecom/pages/auth.page.dart';
-import 'package:exam_ecom/pages/categorie.page.dart';
-import 'package:exam_ecom/pages/categorie_form.page.dart';
-import 'package:exam_ecom/pages/home.page.dart';
-import 'package:exam_ecom/pages/produit_form_page.dart';
-import 'package:exam_ecom/pages/produit_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
